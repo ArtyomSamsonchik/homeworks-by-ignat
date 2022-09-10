c2c26b82121d163f23f45d1fe0c9bd88e53b4c97
ArtyomSamsonchik <colpachek@gmail.com>
2022-09-11 01:46:04 +0300

    Add style to radio buttons container in HW7

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import s from "./HW7.module.css"
 
 const arr = ['x', 'y', 'z']
 
@@ -18,7 +19,7 @@ function HW7() {
                     onChangeOption={onChangeOption}
                 />
             </div>
-            <div>
+            <div className={s.radioContainer}>
                 <SuperRadio
                     name={'radio'}
                     options={arr}
