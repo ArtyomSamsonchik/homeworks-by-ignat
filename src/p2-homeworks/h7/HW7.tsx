22289ba021b947ba34304b95739cb256899642b8
ArtyomSamsonchik <colpachek@gmail.com>
2022-09-11 05:35:22 +0300

    Move SuperRadio and SuperSelect to shared folder

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import SuperSelect from './common/c5-SuperSelect/SuperSelect'
-import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import SuperSelect from '../../common/c5-SuperSelect/SuperSelect'
+import SuperRadio from '../../common/c6-SuperRadio/SuperRadio'
 import s from "./HW7.module.css"
 
 const arr = ['x', 'y', 'z']
